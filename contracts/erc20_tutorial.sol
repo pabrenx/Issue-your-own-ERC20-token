8f06bfef7456db64089e635e92eda2dcbf8cab68
pabrenx <36912668+pabrenx@users.noreply.github.com>
2018-09-09 16:41:45 +0700

    Jembut token
    Jembut token

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.18;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'JEMBUT' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
-// Total supply: 100000000
-// Decimals    : 18
+// Deployed to : 0x591bAF6E533EC738669226C2Ad34207583e94E6a
+// Symbol      : 0JMBT
+// Name        : 0 JEMBUT Token
+// Total supply: 1000000000
+// Decimals    : 8
 //
 // Enjoy.
 //
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract JEMBUTToken is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -112,13 +112,13 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
-    function FucksToken() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
-        decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+    function JEMBUTToken() public {
+        symbol = "0JMBT";
+        name = "0 JEMBUT Token";
+        decimals = 8;
+        _totalSupply = 100000000000000000;
+        balances[0x591bAF6E533EC738669226C2Ad34207583e94E6a] = _totalSupply;
+        Transfer(address(0), 0x591bAF6E533EC738669226C2Ad34207583e94E6a, _totalSupply);
     }
 
 
